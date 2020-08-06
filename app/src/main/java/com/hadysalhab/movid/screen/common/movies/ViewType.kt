4576a3564be10fead09ed3a3560d7ca97c900e88
Hady Salhab <hadysalhab1@gmail.com>
2020-08-06 13:47:09 +1000

    refator: remove MovieCard adapter and viewholder

--- app/src/main/java/com/hadysalhab/movid/screen/common/movies/ViewType.kt
+++
@@ -1,6 +0,0 @@
-package com.hadysalhab.movid.screen.common.movies
-
-enum class ViewType(val viewType: Int) {
-    CARD_MOVIE(1),
-    SEE_ALL(2)
-}
--- app/src/main/java/com/hadysalhab/movid/screen/main/featured/MovieGroupItemViewImpl.kt
+++ app/src/main/java/com/hadysalhab/movid/screen/main/featured/MovieGroupItemViewImpl.kt
@@ -2,44 +2,31 @@ package com.hadysalhab.movid.screen.main.featured
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.hadysalhab.movid.R
+import com.hadysalhab.movid.movies.Movie
 import com.hadysalhab.movid.movies.MovieGroup
 import com.hadysalhab.movid.movies.MovieGroupType
 import com.hadysalhab.movid.screen.common.ViewFactory
-import com.hadysalhab.movid.screen.common.movies.MovieAdapter
+import com.hadysalhab.movid.screen.common.movies.MovieCard
 
 class MovieGroupItemViewImpl(
     layoutInflater: LayoutInflater,
     parent: ViewGroup?,
-    viewFactory: ViewFactory
+    private val viewFactory: ViewFactory
 ) :
-    MovieGroupItemView(), MovieAdapter.Listener {
-    private val recyclerView: RecyclerView
-    private val groupTitle: TextView
-    private val adapter: MovieAdapter
+    MovieGroupItemView(), MovieCard.Listener {
+    private val linearLayout: LinearLayout
     private lateinit var movieGroupType: MovieGroupType
+    private val groupTitle: TextView
 
     init {
-        setRootView(layoutInflater.inflate(R.layout.component_movie_group, parent, false))
-        groupTitle = findViewById(R.id.tv_group_type)
-        recyclerView = findViewById(R.id.rv_movies)
-        adapter = MovieAdapter(
-            this,
-            viewFactory
-        )
-        setupRecyclerView()
+        setRootView(layoutInflater.inflate(R.layout.horizontal, parent, false))
+        linearLayout = findViewById(R.id.movie_linear_layout)
+        groupTitle = findViewById(R.id.group_title)
     }
 
-    private fun setupRecyclerView() {
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            setHasFixedSize(true)
-            adapter = this@MovieGroupItemViewImpl.adapter
-        }
-    }
 
     override fun onMovieCardClicked(movieID: Int) {
         listeners.forEach {
@@ -47,16 +34,25 @@ class MovieGroupItemViewImpl(
         }
     }
 
-    override fun onSeeAllClicked() {
-        listeners.forEach {
-            it.onSeeAllClicked(this.movieGroupType)
-        }
-    }
+//    override fun onSeeAllClicked() {
+//        listeners.forEach {
+//            it.onSeeAllClicked(this.movieGroupType)
+//        }
+//    }
 
 
     override fun displayMovieGroup(movieGroup: MovieGroup) {
-        movieGroupType = movieGroup.movieGroupType
-        groupTitle.text = movieGroupType.value.toUpperCase().split("_").joinToString(" ")
-        adapter.submitList(movieGroup.movies)
+        groupTitle.text = movieGroup.movieGroupType.value.toUpperCase().split("_").joinToString(" ")
+        createMovieCardAndAppend(movieGroup.movies)
+    }
+    private fun createMovieCardAndAppend(movies:List<Movie>){
+        movies.take(5).forEach { movie->
+            val movieCard = viewFactory.getMovieCard(linearLayout)
+            movieCard.registerListener(this)
+            movieCard.displayMovie(movie)
+            linearLayout.addView(movieCard.getRootView())
+        }
+
+
     }
 }
