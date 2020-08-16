e3eee01378121b507648c86b95fd11527da2b8cd
Hady Salhab <hadysalhab1@gmail.com>
2020-08-16 18:20:19 +1000

    feat: clear Movies data after 24 hrs

--- app/src/main/java/com/hadysalhab/movid/movies/MoviesStateManager.kt
+++ app/src/main/java/com/hadysalhab/movid/movies/MoviesStateManager.kt
@@ -1,7 +1,23 @@
 package com.hadysalhab.movid.movies
 
+import android.os.Handler
+
 class MoviesStateManager {
+    init {
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                clearData()
+                handler.postDelayed(this, 24*60*60*1000)
+            }
+        }, 24*60*60*1000)
+    }
+
     val movies = mutableListOf<MovieDetail>()
     var moviesGroup: List<MovieGroup> = emptyList()
     fun areMoviesAvailabe() = moviesGroup.isNotEmpty()
+    private fun clearData() {
+        moviesGroup = emptyList()
+        movies.clear()
+    }
 }
