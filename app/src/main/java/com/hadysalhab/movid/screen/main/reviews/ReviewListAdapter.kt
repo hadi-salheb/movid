892abfad2a8e5ff8ce802ac9654e21c8154f85be
Hady Salhab <hadysalhab1@gmail.com>
2020-10-14 12:56:49 +1100

    refactor: Review adapter and ViewHolder to support loading and error

--- app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListAdapter.kt
+++ app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListAdapter.kt
@@ -5,9 +5,21 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.hadysalhab.movid.movies.Review
 import com.hadysalhab.movid.screen.common.ViewFactory
+import com.hadysalhab.movid.screen.common.paginationerror.PaginationError
 
-class ReviewListAdapter(private val viewFactory: ViewFactory) :
-    ListAdapter<Review, ReviewListViewHolder>(DIFF_CALLBACK) {
+class ReviewListAdapter(
+    private val listener: Listener,
+    private val viewFactory: ViewFactory
+) :
+    ListAdapter<Review, ReviewListViewHolder>(DIFF_CALLBACK),
+    PaginationError.Listener {
+    interface Listener {
+        fun onPaginationErrorClicked()
+    }
+
+    val LOADING = 0
+    val REVIEW = 1
+    val ERROR = 2
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Review>() {
@@ -21,12 +33,37 @@ class ReviewListAdapter(private val viewFactory: ViewFactory) :
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewListViewHolder {
-        val view = viewFactory.getReviewListItem(parent)
-        return ReviewListViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewListViewHolder =
+        when (viewType) {
+            REVIEW -> {
+                val view = viewFactory.getReviewListItem(parent)
+                ReviewListViewHolder.ReviewViewHolder(view)
+            }
+            LOADING -> {
+                val view = viewFactory.getLoadingView(parent)
+                ReviewListViewHolder.LoadingViewHolder(view)
+            }
+            ERROR -> {
+                val view = viewFactory.getPaginationErrorView(parent)
+                view.registerListener(this)
+                ReviewListViewHolder.PaginationErrorViewHolder(view)
+            }
+            else -> {
+                throw RuntimeException("Unsupported viewType $viewType")
+            }
+        }
 
     override fun onBindViewHolder(holder: ReviewListViewHolder, position: Int) {
         holder.bind(getItem(position))
+    }
+
+    override fun getItemViewType(position: Int): Int = when (getItem(position).author) {
+        "LOADING" -> LOADING
+        "ERROR" -> ERROR
+        else -> REVIEW
+    }
+
+    override fun onPaginationErrorClick() {
+        listener.onPaginationErrorClicked()
     }
 }
--- app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListViewHolder.kt
+++ app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListViewHolder.kt
@@ -1,12 +1,28 @@
 package com.hadysalhab.movid.screen.main.reviews
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.hadysalhab.movid.movies.Review
+import com.hadysalhab.movid.screen.common.loading.LoadingView
+import com.hadysalhab.movid.screen.common.paginationerror.PaginationError
 import com.hadysalhab.movid.screen.common.reviews.ReviewListItem
 
-class ReviewListViewHolder(private val reviewListItem: ReviewListItem) :
-    RecyclerView.ViewHolder(reviewListItem.getRootView()) {
-    fun bind(review: Review) {
-        reviewListItem.displayReview(review)
+sealed class ReviewListViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    open fun bind(review: Review) {
+
     }
+
+    class ReviewViewHolder(private val reviewListItem: ReviewListItem) :
+        ReviewListViewHolder(reviewListItem.getRootView()) {
+        override fun bind(review: Review) {
+            super.bind(review)
+            reviewListItem.displayReview(review)
+        }
+    }
+
+    class LoadingViewHolder(loadingView: LoadingView) :
+        ReviewListViewHolder(loadingView.getRootView())
+
+    class PaginationErrorViewHolder(paginationError: PaginationError) :
+        ReviewListViewHolder(paginationError.getRootView())
 }
