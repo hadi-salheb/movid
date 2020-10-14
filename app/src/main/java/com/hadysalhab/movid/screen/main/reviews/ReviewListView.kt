9d47e8594fc3c3782d29038770d79aad869d06fb
Hady Salhab <hadysalhab1@gmail.com>
2020-10-14 12:32:58 +1100

    refactor: add ReviewListState

--- app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListView.kt
+++ app/src/main/java/com/hadysalhab/movid/screen/main/reviews/ReviewListView.kt
@@ -1,7 +1,19 @@
 package com.hadysalhab.movid.screen.main.reviews
 
+import androidx.annotation.DrawableRes
 import com.hadysalhab.movid.movies.Review
 import com.hadysalhab.movid.screen.common.views.BaseObservableViewMvc
+
+data class ReviewListState(
+    val isLoading: Boolean = false,
+    val isPaginationLoading: Boolean = false,
+    val data: List<Review> = emptyList(),
+    @DrawableRes val emptyResultsIconDrawable: Int,
+    val emptyResultsMessage: String,
+    val errorMessage: String? = null,
+    val paginationError: Boolean = false,
+    val title: String = ""
+)
 
 abstract class ReviewListView : BaseObservableViewMvc<ReviewListView.Listener>() {
     interface Listener {
