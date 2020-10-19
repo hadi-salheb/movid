272bf63c96620853d69a251276261d0ecf71c0dd
Hady Salhab <hadysalhab1@gmail.com>
2020-10-19 11:42:44 +1100

    feat: define AccountScreenStateManager

---
+++ app/src/main/java/com/hadysalhab/movid/screen/main/account/AccountScreenStateManager.kt
@@ -0,0 +1,40 @@
+package com.hadysalhab.movid.screen.main.account
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hadysalhab.movid.account.AccountResponse
+
+
+sealed class AccountActions {
+    object Request : AccountActions()
+    data class Success(val accountResponse: AccountResponse? = null) : AccountActions()
+}
+
+
+class AccountScreenStateManager {
+    private val stateLiveData = MutableLiveData<AccountViewState>()
+    private var state: AccountViewState
+        get() {
+            return stateLiveData.value!!
+        }
+        set(value) {
+            stateLiveData.value = value
+        }
+
+    fun setInitialStateAndReturn(initialState: AccountViewState): LiveData<AccountViewState> {
+        stateLiveData.value = initialState
+        return stateLiveData
+    }
+
+    fun dispatch(action: AccountActions) {
+        state = accountStateReducer(action)
+    }
+
+    private fun accountStateReducer(action: AccountActions): AccountViewState = when (action) {
+        AccountActions.Request -> state.copy(loading = true)
+        is AccountActions.Success -> state.copy(
+            loading = false,
+            accountResponse = action.accountResponse
+        )
+    }
+}
