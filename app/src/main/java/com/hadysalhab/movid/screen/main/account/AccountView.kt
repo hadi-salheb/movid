653f960735c18b377b25f8fc88b649bd07004856
Hady Salhab <hadysalhab1@gmail.com>
2020-10-19 11:42:30 +1100

    feat: define AccountViewState

--- app/src/main/java/com/hadysalhab/movid/screen/main/account/AccountView.kt
+++ app/src/main/java/com/hadysalhab/movid/screen/main/account/AccountView.kt
@@ -1,6 +1,12 @@
 package com.hadysalhab.movid.screen.main.account
 
+import com.hadysalhab.movid.account.AccountResponse
 import com.hadysalhab.movid.screen.common.views.BaseObservableViewMvc
+
+data class AccountViewState(
+    val loading: Boolean = false,
+    val accountResponse: AccountResponse? = null
+)
 
 abstract class AccountView : BaseObservableViewMvc<AccountView.Listener>() {
     interface Listener
