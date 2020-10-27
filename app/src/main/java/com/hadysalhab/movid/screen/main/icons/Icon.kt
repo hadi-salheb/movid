2f39a640939ba4d08a6781b62d67929c95b8cdd8
Hady Salhab <hadysalhab1@gmail.com>
2020-10-27 14:17:42 +1100

    fix: change war icon

---
+++ app/src/main/java/com/hadysalhab/movid/screen/main/icons/Icon.kt
@@ -0,0 +1,38 @@
+package com.hadysalhab.movid.screen.main.icons
+
+import androidx.annotation.DrawableRes
+import com.hadysalhab.movid.R
+
+enum class Icon(val author: String, authorUrl: String, @DrawableRes val drawable: Int) {
+    ACTION("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.action),
+    ADVENTURE("DdARA", "https://www.flaticon.com/authors/ddara", R.drawable.adventure),
+    ANIMATION("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.animation),
+    COMEDY("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.comedy),
+    CRIME("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.crime),
+    DOCUMENTARY("srip", "https://www.flaticon.com/authors/srip", R.drawable.documentary),
+    DRAMA("Good Ware", "https://www.flaticon.com/authors/good-ware", R.drawable.drama),
+    FAMILY("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.family),
+    FANTASY("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.fantasy),
+    HISTORY("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.history),
+    HORROR("Good Ware", "https://www.flaticon.com/authors/good-ware", R.drawable.horror),
+    MUSIC("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.music),
+    MYSTERY("xnimrodx", "https://www.flaticon.com/authors/xnimrodx", R.drawable.mystery),
+    ROMANCE(
+        "photo3idea_studio",
+        "https://www.flaticon.com/authors/photo3idea-studio",
+        R.drawable.romance
+    ),
+    SCIENCE_FICTION(
+        "Freepik",
+        "https://www.flaticon.com/authors/freepik",
+        R.drawable.science_fiction
+    ),
+    THRILLER("Kiranshastry", "https://www.flaticon.com/authors/kiranshastry", R.drawable.thriller),
+    WAR("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.war),
+    AUSTRALIA("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.ic_australia),
+    USA("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.ic_united_states),
+    FRANCE("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.ic_france),
+    INDIA("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.ic_india),
+    UK("Freepik", "https://www.flaticon.com/authors/freepik", R.drawable.ic_united_kingdom),
+
+}
